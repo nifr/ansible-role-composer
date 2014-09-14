4d55d7d8d2eb389fa37e2b34972f79cd4cd7ce1d
Nicolai Froehlich <git@nifr.de>
2014-09-14 21:54:26 +0200

    Corrected & improved tests.

--- tests/02-group/spec/composer_spec.rb
+++ tests/02-group/spec/composer_spec.rb
@@ -1,13 +1,15 @@
 require_relative "../../spec_helper"
 # File.expand_path('relative/path', File.dirname(FILE))
 
-describe file('/usr/local/bin/composer') do
+describe file('/opt/composer/composer.phar') do
   it { should be_file }
-  it { should be_mode 755 }
   it { should be_owned_by 'root' }
-  it { should be_grouped_into 'root' }
-#  it { should be_executable.by('root') }
-#  it { should be_executable.by_user('nifr') }
+  it { should be_grouped_into 'testgroup' }
+  it { should be_executable.by_user('root') }
+end
+
+describe file('/usr/local/bin/composer') do
+  it { should be_linked_to '/opt/composer/composer.phar' }
 end
 
 
