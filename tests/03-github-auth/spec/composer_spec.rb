55ff010f0d91e79a0f0edb6cd5333ac10f35ca98
Ramunas Dronga <extern1@dreamcheaper.com>
2015-11-02 19:28:48 +0200

    fixed home path for travis tests

--- tests/03-github-auth/spec/composer_spec.rb
+++ tests/03-github-auth/spec/composer_spec.rb
@@ -1,6 +1,6 @@
 require_relative "../../spec_helper"
 
-describe file('~/.composer/auth.json') do
+describe file(ENV['HOME'] + '/.composer/auth.json') do
     it { should be_file }
     it { should be_owned_by 'root' }
     its(:content) { should match '"github.com": "123"' }
