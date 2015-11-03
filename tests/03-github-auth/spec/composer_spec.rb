725ea9e3218c797ac9a0f4d34d28f86f3d9eb128
Ramunas Dronga <extern1@dreamcheaper.com>
2015-11-03 10:05:55 +0200

    fixed file owner in spec

--- tests/03-github-auth/spec/composer_spec.rb
+++ tests/03-github-auth/spec/composer_spec.rb
@@ -2,6 +2,6 @@ require_relative "../../spec_helper"
 
 describe file(ENV['HOME'] + '/.composer/auth.json') do
     it { should be_file }
-    it { should be_owned_by 'root' }
+    it { should be_owned_by 'travis' }
     its(:content) { should match '"github.com": "123"' }
 end
